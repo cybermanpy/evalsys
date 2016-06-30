05efa26b10ae7251627c92289d3872912af35764
Diego Saldivar <fhersa@gmail.com>
2016-06-29 20:37:58 -0400

    Create user authentication

--- evalsys/urls.py
+++ evalsys/urls.py
@@ -21,4 +21,5 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('years.urls')),
+    url(r'^', include('userprofiles.urls', namespace='userprofiles'),)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ userprofiles/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+from django.contrib.auth import views as auth_views
+urlpatterns = [
+    url(r'^login/$', views.authentication, name='authentication'),
+    url(r'^home/$', views.hello, name="hello"),
+    url(r'^logout$', auth_views.logout, {'next_page': '/login'}, name='logout')
+]
