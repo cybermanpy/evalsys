06e5efabc43c39a554289118629625660c088850
Diego Saldivar <fhersa@gmail.com>
2016-07-04 12:02:23 -0400

    Create view to university models

--- universities/urls.py
+++ universities/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^dashboard/university', views.view_university, name='university'),
+    url(r'^dashboard/universities', views.view_universities, name='universities'),
     # url(r'^dashboard/university/(?P<pk>[0-9]+)/$', views.university_detail, name='detail'),
     url(r'^dashboard/university/new', views.new_university, name='new_university')
 ]
--- universities/views.py
+++ universities/views.py
@@ -1,12 +1,22 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from django.shortcuts import render, get_object_or_404
+#from django.shortcuts import render
 from .models import University
 from .forms import UniversityForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='/')
+def view_universities(request):
+    title = 'Sistema de Evaluacion Institucional'
+    university = University.objects.all()
+    template = loader.get_template('university_view.html')
+    context = {
+        'title': title,
+        'university': university
+    }
+    return HttpResponse(template.render(context, request))
 
 @login_required(login_url='/')
 def new_university(request):
